cea34fb334b51f536953b7a59241f0672a42f578
dabakh <el-hadji-abdoul-aziz.kane@grenoble-inp.org>
2025-04-19 12:39:53 +0200

    ll

--- src/backend/HedgingEngine/MarkatDataReader/EnumIndex.py
+++ src/backend/HedgingEngine/MarkatDataReader/EnumIndex.py
@@ -6,9 +6,9 @@ class EnumIndex(Enum):
     EUROSTOXX50 = "EUROSTOXX50"
     SP500 = "SP500"
     FTSE100 = "FTSE100"
-    # TOPIX = "TOPIX"
-    # ASX200 = "ASX200"
-    NIKKEI='NIKKEI'
+    TOPIX = "TOPIX"
+    ASX200 = "ASX200"
+    # NIKKEI='NIKKEI'
 
     def str_to_enum(name: str) :
         """
@@ -21,20 +21,19 @@ class EnumIndex(Enum):
             EnumIndex: The EnumIndex object.
         """
         match name :
-            # case "ASX200":
-            #     return EnumIndex.ASX200
-
+            case "ASX200":
+                return EnumIndex.ASX200
             case "EUROSTOXX50":
                 return EnumIndex.EUROSTOXX50
             case "FTSE100":
                 return EnumIndex.FTSE100
             case "SP500":
                 return EnumIndex.SP500
-            # case "TOPIX":
-            #     return EnumIndex.TOPIX
+            case "TOPIX":
+                return EnumIndex.TOPIX
 
-            case "NIKKEI":
-                return EnumIndex.NIKKEI
+            # case "NIKKEI":
+            #     return EnumIndex.NIKKEI
             
             case _:
                 raise ValueError(f"Invalid index : {name}")
@@ -46,9 +45,8 @@ index_to_currency = {
     EnumIndex.EUROSTOXX50: EnumCurrency.EUR,
     EnumIndex.SP500: EnumCurrency.USD,
     EnumIndex.FTSE100: EnumCurrency.GBP,
-    # EnumIndex.TOPIX: EnumCurrency.JPY,
-    # EnumIndex.ASX200: EnumCurrency.AUD,
-    EnumIndex.NIKKEI: EnumCurrency.JPY,
-    EnumIndex.FTSE100: EnumCurrency.GBP
+    EnumIndex.TOPIX: EnumCurrency.JPY,
+    EnumIndex.ASX200: EnumCurrency.AUD,
+    # EnumIndex.NIKKEI: EnumCurrency.JPY,
+    # EnumIndex.FTSE100: EnumCurrency.GBP
 }
-
